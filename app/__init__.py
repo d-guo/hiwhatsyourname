be859096c579d008e0c2d039ff88b388e65f7cac
daniel xiong <dxiong2000@gmail.com>
2019-10-26 09:24:46 -0700

    WE'RE IN BETA BOIS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,40 @@
 from flask import Flask
 from flask import render_template, request
-import hashlib
 import base64
+from flask_sqlalchemy import SQLAlchemy
+import qrcode
+from io import BytesIO
+from random import random
+from sqlalchemy import Column, Integer, String 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userinfo.db'
+    app.secret_key = 'thisisthesecretkey'
+
+    db = SQLAlchemy(app)
+
+    class User(db.Model):
+        __tablename__ = "userinfo"
+ 
+        id = db.Column(Integer, primary_key=True)
+        hash_val = db.Column(Integer)
+        name = db.Column(String)
+        age = db.Column(String)
+        gender = db.Column(String)
+        para = db.Column(String)
+        fb = db.Column(String)
+        ig = db.Column(String)
+        snap = db.Column(String)
+        twitter = db.Column(String)
+        image = db.Column(String)
+        
+        def __repr__(self):
+            return "{}".format(self.name)
+
+    db.create_all()
+
 
     @app.route('/')
     def index():
@@ -14,9 +44,16 @@ def create_app():
     def form():
         return render_template('userinput.html')
 
-    @app.route('/result', methods = ['POST'])
-    def result():
-        text_list = list()
+    @app.route('/qr', methods = ['POST'])
+    def qr():
+        counter = (int) (random() * 10000000)
+        print(counter)
+        url = "localhost:5000/page/{}".format(counter)
+        qr_img = qrcode.make(url)
+
+        text_result = request.form
+
+        tl = list()
         """
         order of values:
         name
@@ -30,19 +67,41 @@ def create_app():
         snapchat
         twitter
         """
-
-        text_result = request.form
-
         for key, value in text_result.items():
             if(value is not ''):
-                text_list.append(value)
+                tl.append(value)
             else:
-                text_list.append('NA')
-        #shorthand for text_list
-        tl = text_list
+                tl.append('NA')
 
         file_result = request.files
         file_result = base64.encodestring(file_result['image'].read()).decode('utf-8')
+
+        usr = User(hash_val=counter, name=tl[0], age=tl[1], gender=tl[2], para=tl[3], fb=tl[4], ig=tl[5], snap=tl[6], twitter=tl[7], image=file_result)
+        db.session.add(usr)
+        db.session.commit()
+        
+        buffered = BytesIO()
+        qr_img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+        return render_template('qr.html', file_data=img_str)
+
+
+    @app.route('/page/<hashv>', methods = ['GET', 'POST'])
+    def result(hashv):
+        usr = User.query.filter_by(hash_val = hashv).first()
+
+        tl = list()
+        tl.append(usr.name)
+        tl.append(usr.age)
+        tl.append(usr.gender)
+        tl.append(usr.para)
+        tl.append(usr.fb)
+        tl.append(usr.ig)
+        tl.append(usr.snap)
+        tl.append(usr.twitter)
+
+        file_result = usr.image
 
         return render_template('profile.html', name = tl[0], age = tl[1], gender = tl[2], about_me = tl[3], facebook = tl[4], instagram = tl[5], snapchat = tl[6], twitter = tl[7], file_data = file_result)
 
